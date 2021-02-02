d9d63326c86a79973271e57689d29f396eb3dd82
Renata-Fu <rentafu2333@gmail.com>
2021-02-02 05:47:18 -0500

    adding download script

---
+++ download_packages.R
@@ -0,0 +1,30 @@
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager")
+} else {
+  update.packages("BiocManager")
+}
+
+if (!requireNamespace("fgsea", quietly = TRUE))
+  BiocManager::install("fgsea")
+if (!requireNamespace("BiocParallel", quietly = TRUE))
+  BiocManager::install("BiocParallel")
+if (!requireNamespace("ggplot2", quietly = TRUE))
+  BiocManager::install("ggplot2")
+
+if (!requireNamespace("GSA", quietly = TRUE)) {
+  install.packages("GSA")
+} else {
+  update.packages("GSA")
+}
+
+if (!requireNamespace("data.table", quietly = TRUE)) {
+  install.packages("data.table")
+} else {
+  update.packages("data.table")
+}
+
+if (!requireNamespace("writexl", quietly = TRUE)) {
+  install.packages("writexl")
+} else {
+  update.packages("writexl")
+}
