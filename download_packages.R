b6e2ad3bbedd56667af3d266951bd8cb5a70b07c
Renata-Fu <rentafu2333@gmail.com>
2021-02-02 13:10:31 -0500

    debugged repos issue

--- download_packages.R
+++ download_packages.R
@@ -1,3 +1,7 @@
+r = getOption("repos")
+r["CRAN"] = "http://cran.us.r-project.org"
+options(repos = r)
+
 if (!requireNamespace("BiocManager", quietly = TRUE)) {
   install.packages("BiocManager")
 } else {
